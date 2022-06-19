bab4e3bbbc7667eb7c5c5b007f24920c8ce0507e
Xiye <84062739+Xiyeh@users.noreply.github.com>
2022-06-19 13:38:10 -0700

    Initialized React Native App

--- backend/app.js
+++ backend/app.js
@@ -4,26 +4,26 @@
 const http = require('https') //import https moduke
 
 
-var url = "https://api.spoonacular.com/recipes/findByIngredients/?apiKey=8d51cf1e41ad42fcb9c2699fbba2cfbd&ingredients="
+var url = "https://api.spoonacular.com/recipes/findByIngredients/?apiKey=8d51cf1e41ad42fcb9c2699fbba2cfbd&ingredients=apples"
 //for each ingredient in list
-url = url + 'apples,+sugar,+flour'
-var num = 2;
-url = url + '&number=' + num;
+// url = url + 'apples,+sugar,+flour'
+// var num = 2;
+// url = url + '&number=' + num;
 
 // Sending the request
 //const req = http.request(options, (res) => {
 const req = http.request(url, (res) => {
     let data = ''
-     
+
     res.on('data', (chunk) => {
         data += chunk;
     });
-    
+
     // Ending the response 
     res.on('end', () => {
         console.log('Body:', JSON.parse(data))
     });
-       
+
 }).on("error", (err) => {
     console.log("Error: ", err)
 })
