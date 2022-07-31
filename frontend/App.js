27f8d249030107c2c1e03cb24d538bcbff0f6d60
Aroop Biswal <aroopbiswal@gmail.com>
2022-07-30 20:18:29 -0700

    changed navigation bar

--- frontend/App.js
+++ frontend/App.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
-import SearchRecipe from './components/SearchRecipe';
-import Home from './components/Home';
+import SearchRecipe from './Components/SearchRecipe';
+import Home from './Components/Home';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App = () => {
     return (
-        <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen
           name="SearchRecipe"
           component={SearchRecipe}
           options={{ title: 'Search for Recipe' }}
           />
-        </Stack.Navigator>
+        </Tab.Navigator>
       </NavigationContainer>
     );
   };
